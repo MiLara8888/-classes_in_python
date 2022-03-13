b245fd09f39c89e3b4612a6dafd26010902c6151
Миронова Лара <75615953+MiLara8888@users.noreply.github.com>
2022-03-13 15:15:55 +0300

    Update eq_gt_lt.py

--- eq_gt_lt.py
+++ eq_gt_lt.py
@@ -62,3 +62,48 @@ print(magnus > ian)  # True
 print(magnus < ian)  # False
 print(magnus < [1, 2])  # печатает "Невозможно выполнить сравнениe"
 print(magnus>5)
+
+
+
+
+# Строки в Питоне сравниваются на основании значений символов.
+# Т.е. если мы захотим выяснить, что больше: «Apple» или «Яблоко», – то «Яблоко» окажется бОльшим.
+# А все потому, что английская буква «A» имеет значение 65 (берется из таблицы кодировки), а русская буква «Я» – 1071
+# (с помощью функции ord() это можно выяснить).
+# Такое положение дел не устроило Анну.
+# Она считает, что строки нужно сравнивать по количеству входящих в них символов.
+# Для этого девушка создала класс RealString и реализовала озвученный инструментарий. Сравнивать между собой можно как объекты
+# класса, так и обычные строки с экземплярами класса RealString.
+# К слову, Анне понадобилось только 3 метода внутри класса (включая __init__()) для воплощения задуманного.
+# В общем случае для создания такого класса понадобится 4 метода, так как в Питоне реализованы «богатые» сравнения.
+# Это значит, что если имеется сравнение
+# «больше», то автоматом появится возможность осуществлять сравнение «меньше».
+
+class RealString:
+    def __init__(self, string):
+        self.string = str(string)
+
+    def __eq__(self, other):
+        if not isinstance(other, RealString):
+            return len(self.string) == len(str(other))
+        if isinstance(other, RealString):
+            return len(self.string) == len(other.string)
+
+    def __lt__(self, other):
+        if not isinstance(other, RealString):
+            return len(self.string) < len(str(other))
+        if isinstance(other, RealString):
+            return len(self.string) < len(other.string)
+
+    def __le__(self, other):
+        return self == other or self < other
+
+
+str1 = RealString('Молоко')
+str2 = RealString('Абрикосы растут')
+str3 = 'Золото'
+str4 = [1, 2, 3]
+print(str1 < str4)  # True
+print(str1 >= str2)  # False
+print(str1 == str3)  # True
+
